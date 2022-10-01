932ce718cbad014f27c82a375a13fb0d6014276a
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-01 18:05:44 -0300

    :sparkles:feat(GraphQL): Finished graphql class

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,10 +1,27 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User"
+import crypto, { randomUUID } from 'crypto'
+
+// query: Buscar dados
+// mutation: Criar, alterar ou deletar
 
 @Resolver()
 export class UserResolver {
-  
-  @Query(() => String)
-  async hello() {
-    return 'Hello World'
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user = { id: randomUUID(), name }
+
+    this.data.push(user)
+
+    return user;
   }
 }
